965ea87a55aa40d53be19cde2b9364786181f8f8
尹泽方 <Yinzf01@mingyuanyun.com>
2020-07-18 11:17:50 +0800

    添加需要翻译的城市

---
+++ done.go
@@ -0,0 +1,23 @@
+package main
+
+var provinceMap map[string]string
+var cityMap map[string]string
+
+func doneProvinceMap() {
+	provinceMap["Penza"] = "奔萨"
+	provinceMap["Santa Catarina"] = "圣卡塔琳娜"
+	provinceMap["Gaziantep"] = "加济安泰普"
+	provinceMap["Izmir"] = "伊兹密尔省"
+	provinceMap["La Coruna"] = "拉科鲁尼亚"
+	provinceMap["Maykop"] = "迈科普"
+	provinceMap["Mukdahan"] = "穆达汉(莫拉限)"
+	provinceMap["Ushuaia"] = "乌斯怀亚"
+	provinceMap["Delaware"] = "特拉华"
+	provinceMap["Faro"] = "法罗"
+	provinceMap["Jujuy"] = "胡胡伊"
+	provinceMap["Posadas"] = "波萨达斯"
+}
+
+func doneCityMap() {
+
+}
